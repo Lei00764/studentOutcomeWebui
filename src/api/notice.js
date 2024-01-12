128901dc43eeedc857916f661f9032c8b485b0a8
hugeBlack <wwkq03@outlook.com>
2024-01-12 21:52:44 +0800

    个人通知bug修复

--- src/api/notice.js
+++ src/api/notice.js
@@ -13,7 +13,7 @@ const sendSystemNotice = (content, related_link) => {
 
 const sendPersonalNotice = (user_id, content, related_link) => {
     return request.post("/api/notice/sendPersonalNotice", {
-        user_id, user_id,
+        user_id: user_id,
         content: content,
         related_link: related_link
     })
